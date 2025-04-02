ef6dc49cca772bfff53e2448ebdbb7feb852c182
LeTrungPhong <letrungphong22122004@gmail.com>
2025-04-02 18:03:09 +0700

    [Feat]: add border for obstacle

--- Assets/Scripts/ObstacleManager.cs
+++ Assets/Scripts/ObstacleManager.cs
@@ -19,6 +19,9 @@ public class ObstacleManager : MonoBehaviour
     private GameObject golfBall;
     private float changeDirectionSize = 1;
     public float obstacleSize = 1;
+    public float obstacleSizeX = 1;
+    public float obstacleSizeY = 1;
+    public float obstacleSizeZ = 1;
     public float scaleSize = 0;
     public float ballSize = 0.4f;
     public int highFly = 3;
@@ -33,6 +36,9 @@ public class ObstacleManager : MonoBehaviour
     public int[][] spawnGoal;
     public int[][] spawnBall;
     public int[][] hint;
+
+    Color lowColor = new Color(106f / 255, 106f / 255, 106f / 255, 1f);
+    Color highColor = new Color(55f / 255, 55f / 255, 55f / 255, 1f);
 
     private void Awake()
     {
@@ -58,7 +64,7 @@ public class ObstacleManager : MonoBehaviour
             {
                 if (this.spawnObstacles[i][j] > 0)
                 {
-                    this.SpawnObstacle(this.spawnObstacles[i][j], i, j);
+                    this.SpawnObstacle(i, this.spawnObstacles[i][j], j);
                 }
                 if (this.spawnPlanes[i][j] > 0)
                 {
@@ -99,14 +105,163 @@ public class ObstacleManager : MonoBehaviour
         }
     }
 
-    public void SpawnObstacle(int high, int indexX, int indexY)
+    public void SpawnObstacle(int indexX, int high, int indexZ)
     {
         for (int i = 0; i < high; ++i)
         {
-            Instantiate(obstaclePrefab, new Vector3(indexX * obstacleSize, i * obstacleSize, indexY * obstacleSize), Quaternion.identity);
+            GameObject obstacle = Instantiate(obstaclePrefab, new Vector3(indexX * obstacleSize, i * obstacleSize, indexZ * obstacleSize), Quaternion.identity);
+
+            //if (!checkObstacle(new int[] { indexX, i + 1, indexZ }))
+            //{
+            //    if (checkObstacle(new int[] { indexX + 1, i + 1, indexZ }) || checkObstacle(new int[] { indexX + 1, i, indexZ }))
+            //    {
+            //        DrawLine(obstacle, 0, 1, lowColor);
+            //    } else
+            //    {
+            //        DrawLine(obstacle, 0, 1, highColor);
+            //    }
+            //}
+
+
+
+            //if (!checkObstacle(new int[] { indexX, i + 1, indexZ }))
+            //{
+            //    if (checkObstacle(new int[] { indexX, i + 1, indexZ - 1 }) || checkObstacle(new int[] { indexX, i, indexZ - 1 }))
+            //    {
+            //        DrawLine(obstacle, 1, 2, lowColor);
+            //    } else
+            //    {
+            //        DrawLine(obstacle, 1, 2, highColor);
+            //    }
+            //}
+
+
+
+            //if (!checkObstacle(new int[] { indexX, i + 1, indexZ }))
+            //{
+            //    if (checkObstacle(new int[] { indexX - 1, i + 1, indexZ }) || checkObstacle(new int[] { indexX - 1, i, indexZ }))
+            //    {
+            //        DrawLine(obstacle, 2, 3, lowColor);
+            //    } else
+            //    {
+            //        DrawLine(obstacle, 2, 3, highColor);
+            //    }
+            //}
+
+
+
+            //if (!checkObstacle(new int[] { indexX, i + 1, indexZ }))
+            //{
+            //    if (checkObstacle(new int[] { indexX, i + 1, indexZ + 1 }) || checkObstacle(new int[] { indexX, i, indexZ + 1 }))
+            //    {
+            //        DrawLine(obstacle, 3, 0, lowColor);
+            //    } else
+            //    {
+            //        DrawLine(obstacle, 3, 0, highColor);
+            //    }
+            //}
+            HandleDrawLine(0, obstacle, new int[] { indexX, i, indexZ }, new int[] { 1, 1, 0 }, 0, 1);
+            HandleDrawLine(0, obstacle, new int[] { indexX, i, indexZ }, new int[] { 0, 1, -1 }, 1, 2);
+            HandleDrawLine(0, obstacle, new int[] { indexX, i, indexZ }, new int[] { -1, 1, 0 }, 2, 3);
+            HandleDrawLine(0, obstacle, new int[] { indexX, i, indexZ }, new int[] { 0, 1, 1 }, 3, 0);
+
+            HandleDrawLine(0, obstacle, new int[] { indexX, i, indexZ }, new int[] { 1, -1, 0 }, 4, 5);
+            HandleDrawLine(0, obstacle, new int[] { indexX, i, indexZ }, new int[] { 0, -1, -1 }, 5, 6);
+            HandleDrawLine(0, obstacle, new int[] { indexX, i, indexZ }, new int[] { -1, -1, 0 }, 6, 7);
+            HandleDrawLine(0, obstacle, new int[] { indexX, i, indexZ }, new int[] { 0, -1, 1 }, 7, 4);
+
+            //DrawLine(obstacle, 4, 5);
+            //DrawLine(obstacle, 5, 6);
+            //DrawLine(obstacle, 6, 7);
+            //DrawLine(obstacle, 7, 4);
+
+            HandleDrawLine(1, obstacle, new int[] { indexX, i, indexZ }, new int[] { 1, 0, 1 }, 0, 4);
+            HandleDrawLine(1, obstacle, new int[] { indexX, i, indexZ }, new int[] { 1, 0, -1 }, 1, 5);
+            HandleDrawLine(1, obstacle, new int[] { indexX, i, indexZ }, new int[] { -1, 0, -1 }, 2, 6);
+            HandleDrawLine(1, obstacle, new int[] { indexX, i, indexZ }, new int[] { -1, 0, 1 }, 3, 7);
+
+            //DrawLine(obstacle, 0, 4);
+            //DrawLine(obstacle, 1, 5);
+            //DrawLine(obstacle, 2, 6);
+            //DrawLine(obstacle, 3, 7);
         }
     }
 
+    public void HandleDrawLine(int typeCheck, GameObject obstacle, int[] post, int[] indexCheck, int startLine, int endLine)
+    {
+        if (typeCheck == 0)
+        {
+            if (!checkObstacle(new int[] { post[0], post[1] + indexCheck[1], post[2] }))
+            {
+                if (checkObstacle(new int[] { post[0] + indexCheck[0], post[1] + indexCheck[1], post[2] + indexCheck[2] }) || checkObstacle(new int[] { post[0] + indexCheck[0], post[1], post[2] + indexCheck[2] }))
+                {
+                    DrawLine(obstacle, startLine, endLine, lowColor);
+                }
+                else
+                {
+                    DrawLine(obstacle, startLine, endLine, highColor);
+                }
+            }
+        } else if (typeCheck == 1)
+        {
+            if (
+                !checkObstacle(new int[] { post[0] + indexCheck[0], post[1], post[2] })
+                && !checkObstacle(new int[] { post[0] + indexCheck[0], post[1], post[2] + indexCheck[2] })
+                && !checkObstacle(new int[] { post[0], post[1], post[2] + indexCheck[2] })
+                )
+            {
+                DrawLine(obstacle, startLine, endLine, highColor);
+            } else
+            {
+                DrawLine(obstacle, startLine, endLine, lowColor);
+            }
+        }
+    }
+
+    public void DrawLine(GameObject obstacle, int pointStart, int pointEnd, Color color)
+    {
+        GameObject lineObject = new GameObject("Line");
+        lineObject.transform.SetParent(obstacle.transform);
+        LineRenderer line = lineObject.AddComponent<LineRenderer>();
+        //line.transform.SetParent(obstacle.transform);
+        line.startWidth = 0.02f;
+        line.endWidth = 0.02f;
+        //line.positionCount = 16;
+        //line.loop = true;
+        line.material = new Material(Shader.Find("Sprites/Default"));
+        line.startColor = color;
+        line.endColor = color;
+
+        Vector3 size = new Vector3(obstacleSizeX, obstacleSizeY, obstacleSizeZ) / 2;
+
+        Vector3[] points = new Vector3[]
+        {
+                new Vector3(size.x, size.y, size.z),
+                new Vector3(size.x, size.y, -size.z),
+                new Vector3(-size.x, size.y, -size.z),
+                new Vector3(-size.x, size.y, size.z),
+                new Vector3(size.x, -size.y, size.z),
+                new Vector3(size.x, -size.y, -size.z),
+                new Vector3(-size.x, -size.y, -size.z),
+                new Vector3(-size.x, -size.y, size.z)
+        };
+
+        int[] edges = {
+                pointStart, pointEnd
+            };
+
+        Vector3[] linePositions = new Vector3[edges.Length];
+
+        for (int j = 0; j < edges.Length; j++)
+        {
+            float gap = 0.001f;
+            linePositions[j] = obstacle.transform.position + points[edges[j]] 
+                + new Vector3(points[edges[j]].x > 0 ? gap : -gap, points[edges[j]].y > 0 ? gap : -gap, points[edges[j]].z > 0 ? gap : -gap);
+        }
+
+        line.positionCount = linePositions.Length;
+        line.SetPositions(linePositions);
+    }
 
     // Update is called once per frame
     void Update()
